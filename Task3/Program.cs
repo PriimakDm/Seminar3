6fe883d71d3c3be8539a197127c7b7464b520052
Dmitrii Priimak <priimak69@inbox.ru>
2023-06-19 01:35:28 +0700

    Task3

--- Task3/Program.cs
+++ Task3/Program.cs
@@ -1,3 +1,35 @@
-﻿// Напишите программу, которая принимает на вход число (N) 
-// и выдаёт таблицу кубов чисел от 1 до N.
-Cosole.WriteLine("Hello, World!");
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // Напишите программу, которая принимает на вход число (N) 
+        // и выдаёт таблицу кубов чисел от 1 до N.
+        void Cube(int numN)
+        {
+            int current = 1;
+            while (current <= numN)
+            {
+                double result = Math.Pow(current, 3);
+                Console.Write($"{result:f0}, ");
+                current++;
+            }
+            Console.WriteLine("\b\b.");
+        }
+        void CubeNeg(int numN)
+        {
+            int current = 1;
+            while (current >= numN)
+            {
+                double result = Math.Pow(current, 3);
+                Console.Write($"{result:f0}, ");
+                current--;
+            }
+            Console.WriteLine("\b\b.");
+        }
+        Console.Write("Input namber: ");
+        int nam = Convert.ToInt32(Console.ReadLine());
+        if (nam > 0)
+            Cube(nam);
+        else CubeNeg(nam);
+    }
+}
