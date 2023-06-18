f0e52a2e58fdf6a64bf406ef42edc050eadf3a6f
Dmitrii Priimak <priimak69@inbox.ru>
2023-06-18 23:34:33 +0700

    TaskFolders

--- Task2/Program.cs
+++ Task2/Program.cs
@@ -1,3 +1,9 @@
 ﻿// Напишите программу, которая принимает на вход координаты двух точек 
 // и находит расстояние между ними в 3D пространстве.
-Console.WriteLine("Hello, World!");
+//            _____________________________
+// формула d=V(x1-x2)кв+(y1-y2)кв+(z1-z2)кв
+double Distance (double x1, double x2, double y1, double y2, double z1, double z2)
+{
+    double dist = Math.Sqrt(x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)+(z2-z1)*(z2-z1);
+    return dist;
+}
